2be982ff2a0ee12e03c7ff3dd77e6b4b48b8ae2a
Granth Agarwal <heygranth@gmail.com>
2025-06-30 16:41:21 +0530

    navbar and styling

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,5 +1,27 @@
-import "react"
+import "react";
+import {SignedIn, SignedOut, UserButton} from "@clerk/clerk-react";
+import {Link, Navigate, Outlet } from 'react-router-dom';
 
 export function Layout() {
-    return <></>
+    return <div className="app-layout">
+        <header className="app-header">
+            <h1>Code Challenge Generator</h1>
+            <nav>
+                <SignedIn>
+                    <Link to="/">Generate Challenge</Link>
+                    <Link to="/history">History</Link>
+                    <UserButton/>
+                </SignedIn>
+            </nav>
+        </header>
+
+        <main className="app-main">
+            <SignedOut>
+                <Navigate to="/sign-in" replace/>
+            </SignedOut>
+            <SignedIn>
+                <Outlet />
+            </SignedIn>
+        </main>
+    </div>
 }
